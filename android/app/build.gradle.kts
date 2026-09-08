39a8ea610e48b7ff65cb18c6d572b355a1c13a81
mathe <mathewamalmathew@gmail.com>
2026-09-08 11:46:59 +0530

    feat: initialize Android Gradle project structure and configuration

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,8 @@ plugins {
 android {
     namespace = "com.example.note_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "28.2.13676358"
+
 
     compileOptions {
         isCoreLibraryDesugaringEnabled = true
